83c782b60c77962a36a505bf30be7c64e5e9267d
Ross Byrne <ross.byrne@outlook.ie>
2026-06-25 17:44:49 +0200

    Updated keymap for copying relative/absolute file paths

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,28 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      mappings = {
+        -- Copy path relative to cwd
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            local relative = vim.fn.fnamemodify(path, ":.")
+            vim.fn.setreg("+", relative, "c")
+          end,
+          desc = "Copy Relative Path to Clipboard",
+        },
+        -- Copy absolute path (previous Y behavior)
+        ["gy"] = {
+          function(state)
+            local node = state.tree:get_node()
+            vim.fn.setreg("+", node:get_id(), "c")
+          end,
+          desc = "Copy Absolute Path to Clipboard",
+        },
+      },
+    },
     filesystem = {
       filtered_items = {
         visible = false,
